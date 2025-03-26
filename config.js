bcac0d1a57d9cf82592d2a846ad2ce9c98755b11
KishoriRaut <kishoriraut369@gmail.com>
2025-03-26 16:28:59 +0545

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://gdczwaetelzhkekypcbv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkY3p3YWV0ZWx6aGtla3lwY2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5ODQzNzksImV4cCI6MjA1ODU2MDM3OX0.YlR6t9TYIG8Lvu2B65VS_V0xIBAKtnLT5qWfaiRkvlk';
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
